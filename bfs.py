0f86fc731b72529d31cdc47dddd94d23bd981d7b
baileytom <baileytb7@gmail.com>
2017-10-21 12:02:40 -0400

    first commit

---
+++ bfs.py
@@ -0,0 +1,87 @@
+from implementation import *
+
+def breadth_first_search_1(graph, start):
+    # print out what we find
+    frontier = Queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+    while not frontier.empty():
+        current = frontier.get()
+        print("Visiting %r" % current)
+        for next in graph.neighbors(current):
+            if next not in visited:
+                frontier.put(next)
+                visited[next] = True
+
+def breadth_first_search_2(graph, start):
+    # return "came_from"
+    frontier = Queue()
+    frontier.put(start)
+    came_from = {}
+    came_from[start] = None
+
+    while not frontier.empty():
+        current = frontier.get()
+        for next in graph.neighbors(current):
+            if next not in came_from:
+                frontier.put(next)
+                came_from[next] = current
+
+    return came_from
+
+def breadth_first_search_3(graph, start, goal):
+    frontier = Queue()
+    frontier.put(start)
+    came_from = {}
+    came_from[start] = None
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            if next not in came_from:
+                frontier.put(next)
+                came_from[next] = current
+
+    return came_from
+    
+def dijkstra_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+
+
+
+#breadth_first_search_1(example_graph, 'A')
+
+g = SquareGrid(30, 15)
+g.walls = DIAGRAM1_WALLS
+
+parents = a_star_search(g, (8, 7), (17, 2))
+draw_grid(g, width=2, point_to=parents, start=(8, 7), goal=(17, 2))
---
+++ client.py
@@ -0,0 +1,199 @@
+#!/usr/bin/python
+
+import sys
+import json
+import random
+import 
+
+import implementation
+
+
+if (sys.version_info > (3, 0)):
+    print("Python 3.X detected")
+    import socketserver as ss
+else:
+    print("Python 2.X detected")
+    import SocketServer as ss
+
+
+class NetworkHandler(ss.StreamRequestHandler):
+    def handle(self):
+        game = Game()
+
+        while True:
+            data = self.rfile.readline().decode() # reads until '\n' encountered
+            json_data = json.loads(str(data))
+            # uncomment the following line to see pretty-printed data
+            # print(json.dumps(json_data, indent=4, sort_keys=True))
+            #response = game.get_random_move(json_data).encode()
+            response = game.get_moves(json_data).encode()
+            self.wfile.write(response)
+
+
+
+class Game:
+    def __init__(self):
+        self.units = set() # set of unique unit ids
+        self.directions = ['N', 'S', 'E', 'W']
+        self.map = Map()
+        #self.map.show_map()
+
+        # Game stages
+        # Stage 1: Gather & search
+        #   - Mass produce scouts
+        #   - Scouts in search mode, target unknown space
+        # Stage 2: (Found base) Attack
+        #   - All units attack
+
+        #for
+
+        self.stage = 1
+
+    def get_random_move(self, json_data):
+        units = set([unit['id'] for unit in json_data['unit_updates'] if unit['type'] != 'base'])
+        self.units |= units # add any additional ids we encounter
+        unit = random.choice(tuple(self.units))
+        direction = random.choice(self.directions)
+        move = 'MOVE'
+        command = {"commands": [{"command": move, "unit": unit, "dir": direction}]}
+        response = json.dumps(command, separators=(',',':')) + '\n'
+        return response
+
+    def respond(self, json_data):
+        units = set([unit['id'] for unit in json_data['unit_updates'] if unit['type'] != 'base'])
+        self.units |= units  # add any additional ids we encounter
+        #for unit in units:
+        #for unit in units:
+        #    if self._units[unit['id']] == None:
+
+    def heuristic(a, b):
+        (x1, y1) = a
+        (x2, y2) = b
+        return abs(x1 - x2) + abs(y1 - y2)
+
+    def a_star_search(graph, start, goal):
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == goal:
+                break
+
+            for next in graph.neighbors(current):
+                new_cost = cost_so_far[current] + graph.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + heuristic(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+
+        return came_from, cost_so_far
+
+    def get_moves(self, json_data):
+        unit_updates = json_data['unit_updates']
+        units = set([unit['id'] for unit in json_data['unit_updates'] if unit['type'] != 'base'])
+        self.units |= units
+
+        #print(json_data['tile_updates'])
+
+        for tile in json_data['tile_updates']:
+            if not tile['visible']:
+                continue
+
+            #print(str(tile) + "\n")
+            # Tile
+            # x, y, visible, blocked, resources : {id, type, total, value?}
+            if tile['blocked']:
+                    self.map.settile(tile['x'], tile['y'], 1)
+            try:
+                #print(tile['resources'])
+                if tile['resources'] != None:
+                    self.map.settile(tile['x'], tile['y'], "r")
+                    self.map.resources.append((tile['x'], tile['y']))
+            except:
+                pass
+
+
+
+        for unit in unit_updates:
+            if self.stage == 1:
+                if unit['type'] == 'worker':
+                    unit['target'] = self.map.resources[0]
+
+                    # Do worker stuff
+                elif unit['type'] == 'scout':
+                    pass
+                    # Do scout stuff
+            elif self.stage == 2:
+                if unit['type'] == 'worker':
+                    pass
+                    # Do worker stuff
+                elif unit['type'] == 'scout':
+                    pass
+                    # Scout
+
+        self.map.show_map()
+
+        unit = random.choice(tuple(self.units))
+        direction = random.choice(self.directions)
+        move = 'MOVE'
+        command = {"commands": [{"command": move, "unit": unit, "dir": direction}]}
+        response = json.dumps(command, separators=(',', ':')) + '\n'
+        return response
+
+
+
+class Map:
+    def __init__(self):
+        self.grid = [[0 for y in range(0,60)] for x in range(0, 60)]
+        self.originx = 29
+        self.originy = 29
+        self.grid[self.originx][self.originy] = 1
+
+        self.resources = []
+
+    def show_map(self):
+        print(self.resources)
+        for x in self.grid:
+            print_str = ""
+            for y in x:
+                print_str += ("{} ".format(y))
+            print(print_str)
+
+    def settile(self, x, y, value):
+        self.grid[x+self.originx][y+self.originy] = value
+
+    def gettile(self, x, y):
+        return self.grid[x+self.originx][y+self.originy]
+"""
+
+class Unit:
+    def __init__(self):
+        self.x, self.y = 0, 0;
+
+class Worker(Unit):
+    def __init__(self):
+        super.__init__(self)
+
+    def get_move(self, game):
+
+class Tank(Unit):
+    def __init__(self):
+        super.__init__(self)
+        #self.hp =
+    #def
+
+"""
+if __name__ == "__main__":
+    port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 9090
+    host = '10.8.2.42'
+
+    server = ss.TCPServer((host, port), NetworkHandler)
+    print("listening on {}:{}".format(host, port))
+    server.serve_forever()
